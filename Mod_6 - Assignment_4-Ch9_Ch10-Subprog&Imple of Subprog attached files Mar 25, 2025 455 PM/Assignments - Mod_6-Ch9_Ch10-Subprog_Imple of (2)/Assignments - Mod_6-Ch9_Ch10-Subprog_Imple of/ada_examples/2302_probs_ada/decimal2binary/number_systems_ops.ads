c5d85e0be5db2dd32d67b17e5a6fa4e060186e46
Wyatt Bramblett <143138924+WyattBram@users.noreply.github.com>
2025-03-25 18:36:39 -0400

    Delete Mod_6 - Assignment_4-Ch9_Ch10-Subprog&Imple of Subprog attached files Mar 25, 2025 455 PM directory

--- Mod_6 - Assignment_4-Ch9_Ch10-Subprog&Imple of Subprog attached files Mar 25, 2025 455 PM/Assignments - Mod_6-Ch9_Ch10-Subprog_Imple of (2)/Assignments - Mod_6-Ch9_Ch10-Subprog_Imple of/ada_examples/2302_probs_ada/decimal2binary/number_systems_ops.ads
+++
@@ -1,5 +0,0 @@
-package Number_Systems_Ops is
-
-   function decimal_to_binary (n: in positive) return String;
-
-end Number_Systems_Ops;
--- Mod_6 - Assignment_4-Ch9_Ch10-Subprog&Imple of Subprog attached files Mar 25, 2025 455 PM/Assignments - Mod_6-Ch9_Ch10-Subprog_Imple of (2)/Assignments - Mod_6-Ch9_Ch10-Subprog_Imple of/ada_examples/2302_probs_ada/equal_arrays/array_ops.ads
+++
@@ -1,7 +0,0 @@
-package Array_Ops is
-
-   type Data_Type is array (Positive range <>) of Integer;
-
-   function Are_Equal (a1, a2: in Data_Type) return Boolean;
-
-end Array_Ops;
